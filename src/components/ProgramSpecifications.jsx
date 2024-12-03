63e46e91200e6851ae72ddd7f672d66340d03795
teuku <teukusalmanfarizi2003@gmail.com>
2024-12-03 09:05:54 +0700

    add gh-pages

--- src/components/ProgramSpecifications.jsx
+++ src/components/ProgramSpecifications.jsx
@@ -3,9 +3,8 @@ function ProgramSpecifications() {
     <div className="flex-1 mb-4 text-gray-400 overflow-y-auto border border-gray-700 rounded p-4 max-h-[300px]">
       <strong>Program Specifications</strong>
       <p className="mt-2 text-sm">
-        Masukkan detail spesifikasi yang dibutuhkan. Anda dapat
-        menambahkan spesifikasi lebih banyak jika diperlukan, dan konten
-        ini akan dapat di-scroll secara vertikal.
+        1. Menggunakan 1 konsep OOP(Selain Konsep Obeject) 30 POIN <b/>
+        2. Menggunakan One Liner 15 POINT
       </p>
     </div>
   );
