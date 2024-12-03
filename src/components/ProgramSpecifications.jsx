48ee65e6b3687d416ebebfc7e878687f033ed9f0
teuku <teukusalmanfarizi2003@gmail.com>
2024-12-03 09:14:37 +0700

    add gh-pages

--- src/components/ProgramSpecifications.jsx
+++ src/components/ProgramSpecifications.jsx
@@ -3,7 +3,7 @@ function ProgramSpecifications() {
     <div className="flex-1 mb-4 text-gray-400 overflow-y-auto border border-gray-700 rounded p-4 max-h-[300px]">
       <strong>Program Specifications</strong>
       <p className="mt-2 text-sm">
-        <div>1. Menggunakan 1 konsep OOP(Selain Konsep Obeject) 30 POIN</div>
+        <div className="mb-5">1. Menggunakan 1 konsep OOP(Selain Konsep Obeject) 30 POIN</div>
         <div>2. Menggunakan One Liner 15 POINT</div>
       </p>
     </div>
